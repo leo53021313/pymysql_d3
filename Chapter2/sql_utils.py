5d24b789c50b47bcd84f610b77dde02400736268
leo53021313 <leo53021313@gmail.com>
2026-05-07 21:49:45 +0800

    5/7上課

--- Chapter2/sql_utils.py
+++ Chapter2/sql_utils.py
@@ -4,7 +4,56 @@ import configparser
 config = configparser.ConfigParser()
 config.read('../Chapter1/config.ini')
 
-# 查詢使用者的 function
+class SqlManager():
 
 
-# 建立寫入使用者的 function
+    def __init__(self, database = None):
+        self.connection = pymysql.connect(
+            host=config.get('DB', 'host'),
+            user=config.get('DB', 'user'),
+            password=config.get('DB', 'password'),
+            port=config.getint('DB', 'port'),
+            cursorclass=pymysql.cursors.DictCursor,
+            database=database
+        )
+    
+    def create_database(self, database):
+        sql = f"""
+            CREATE DATABASE IF NOT EXISTS {database};
+        """
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql)
+
+        self.dbs
+
+    @property
+    def dbs(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("SHOW DATABASES;")
+            dbs = cursor.fetchall()
+
+        print(dbs)
+
+
+    # 查詢使用者的 function
+    def check_user(self, username, password):
+        with self.connection.cursor() as cursor:
+            sql = """
+                SELECT * FROM user WHERE username = %s AND password = %s
+            """
+            cursor.execute(sql, (username, password))
+            result = cursor.fetchall()
+        print(f"\033[31m使用者: {result}\033[0m")
+
+
+    # 建立寫入使用者的 function
+    def create_user(self, name, age, username, password):
+        with self.connection.cursor() as cursor:
+            sql = """
+                INSERT INTO user (name, age, username, password)
+                VALUES (%s, %s, %s, %s)
+            """
+            cursor.execute(sql, (name, age, username, password))
+
+        self.connection.commit()
+        print(f"\033[31m成功寫入使用者: {username}\033[0m")
